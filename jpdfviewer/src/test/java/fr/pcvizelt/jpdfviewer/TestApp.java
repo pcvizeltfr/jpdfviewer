d59dd3d85b58dfb76330e318279fc92e71ef3892
Corentin Pierre <corentinpierre89@gmail.com>
2024-10-14 13:43:59 +0200

    Update TestApp.java

--- jpdfviewer/src/test/java/fr/pcvizelt/jpdfviewer/TestApp.java
+++ jpdfviewer/src/test/java/fr/pcvizelt/jpdfviewer/TestApp.java
@@ -7,10 +7,10 @@ import javax.swing.JFrame;
 public class TestApp
 {
     public static void main(final String[] args) {
-        final File pdfFile = new File(args[0]);
-        final JFrame frame = new JFrame();
+        File pdfFile = new File(args[0]);
+        JFrame frame = new JFrame();
         frame.setTitle("JPDFViewer");
-        final JPDFViewer pdfViewer = new JPDFViewer(pdfFile);
+        JPDFViewer pdfViewer = new JPDFViewer(pdfFile);
         pdfViewer.getView().setPreviewDotsPerInch(150);
         frame.setSize(1280, 720);
         frame.add(pdfViewer);
