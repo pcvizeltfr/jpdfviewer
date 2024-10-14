556b55c96784b70edcb78e72c15a951f8b337fbc
Corentin <Corentin@138.195.60.42>
2024-10-14 14:45:44 +0200

    chan

--- jpdfviewer/src/test/java/fr/pcvizelt/jpdfviewer/TestApp.java
+++ jpdfviewer/src/test/java/fr/pcvizelt/jpdfviewer/TestApp.java
@@ -6,15 +6,10 @@ import javax.swing.JFrame;
 
 public class TestApp
 {
-<<<<<<< HEAD
     public static void main(String[] args) {
     	args = new String[] { "D:/Cours/2A Centrale/Projet S7/calcul lagrangien v2/formulation faible.pdf" };
         
     	File pdfFile = new File(args[0]);
-=======
-    public static void main(final String[] args) {
-        File pdfFile = new File(args[0]);
->>>>>>> branch 'main' of https://github.com/pcvizeltfr/jpdfviewer.git
         JFrame frame = new JFrame();
         frame.setTitle("JPDFViewer");
         JPDFViewer pdfViewer = new JPDFViewer(pdfFile);
